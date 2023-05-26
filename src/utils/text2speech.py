01b0c7f43a1cf5e25428aa4d787c7a176a69cc3f
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-26 15:55:48 +0530

    Update text2speech.py

--- src/utils/text2speech.py
+++ src/utils/text2speech.py
@@ -1,7 +1,8 @@
 import os
 import tempfile
 from TTS.api import TTS
-
+import openai
+openai.api_key = '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'
 
 
 class TTSTalker():
@@ -15,7 +16,15 @@ class TTSTalker():
                 delete = False,
                 suffix = ('.'+'wav'),
             )
-
-        self.tts.tts_to_file(text, speaker=self.tts.speakers[0], language=language, file_path=tempf.name)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": f"imagine you are karl max and reply to me for this question: {text} in 15 words"}
+            ]
+        )
+        OP = response['choices'][0]['message']['content']
+        OP = OP.replace('"', '')
+    
+        self.tts.tts_to_file(OP, speaker=self.tts.speakers[3], language=language, file_path=tempf.name)
 
         return tempf.name
